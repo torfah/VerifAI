d1282cac42f5f35ff3cf14c3afe56a94d33ec582
Edward Kim <ek65@berkeley.edu>
2021-03-25 19:34:52 -0700

    correct lane_invasion sensor name

--- src/verifai/simulators/carla/carla_world.py
+++ src/verifai/simulators/carla/carla_world.py
@@ -510,7 +510,7 @@ class LaneInvasionSensor(object):
         self._hud = hud
         self._history = []
         world = self._parent.get_world()
-        bp = world.get_blueprint_library().find('sensor.other.lane_detector')
+        bp = world.get_blueprint_library().find('sensor.other.lane_invasion')
         self.sensor = world.spawn_actor(bp, carla.Transform(), attach_to=self._parent)
         # We need to pass the lambda a weak reference to self to avoid circular
         # reference.
