5430fcbac5d8f1cd2ce4a34e0798d98f2f32530e
john <johnathanchewy@gmail.com>
2021-09-06 18:13:22 -0700

    fix pruning to projecting, add new columns instead of modify

--- src/verifai/mapper/mapper.py
+++ src/verifai/mapper/mapper.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 class Mapper:
@@ -15,59 +16,21 @@ class Mapper:
     def applyMapping(self):
         mappedTable = self.table.copy()
         for function in self.functions:
-            assert function.mod in self.table.columns, 'Attemping to modify undefined column in table'
-            # create new column instead
+            mappedTable[function.modify] = np.zeros((len(mappedTable),))
             for idx, _ in df.iterrows():
-                mappedTable.at[idx, function.mod] = function.apply(idx, self.table)
+                mappedTable.at[idx, function.modify] = function.apply(idx, self.table)
 
-        for project in self.project:
-            assert project in self.table, 'Attempting to project undefined column in table'
-            del mappedTable[project]
+        toPrune = list(set(mappedTable.columns).difference(self.project))
+        for prune in toPrune:
+            assert prune in mappedTable, 'Attempting to project undefined column in table'
+            del mappedTable[prune]
 
         return mappedTable
 
 
 class TableFunctions:
-
-    def apply(self, idx, table):
-        return table[idx, 'colMod']
-
-
-class SumColumns(TableFunctions):
     def __init__(self, modify):
-        super(SumColumns, self).__init__()
-        self.mod = modify
+        self.modify = modify
 
     def apply(self, idx, table):
-        return table.at[idx, 'x'] + table.at[idx, 'y']
-
-
-class SumPreviousRow(TableFunctions):
-    def __init__(self, modify):
-        super(SumPreviousRow, self).__init__()
-        self.mod = modify
-
-    def apply(self, idx, table):
-        if idx > 0:
-            return table.at[idx, 'x'] + table.at[idx - 1, 'x']
-        return table.at[idx, 'x']
-
-
-df = pd.DataFrame(index=list(range(3)), columns=['x','y'])
-df.at[0, 'x'] = 2
-df.at[0, 'y'] = 1
-df.at[1, 'x'] = 7
-df.at[1, 'y'] = 4
-df.at[2, 'x'] = 6
-df.at[2, 'y'] = 3
-print(df)
-
-mapper = Mapper(df)
-col_sum = SumColumns('x')
-row_sum = SumPreviousRow('x')
-mapper.defineTransformFunctions(col_sum)
-mapper.defineTransformFunctions(row_sum)
-mapper.defineProjectFunctions(['y']) # to keep not to remove
-new_df = mapper.applyMapping()
-
-print(new_df)
+        return table[idx, modify]
